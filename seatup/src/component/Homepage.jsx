b79ee1ca78652d9bfcbe7f1d26e71137bd9cb925
Faiz Jazadi <me@lcat.dev>
2022-11-21 22:14:46 +0700

    [wip] Homepage
    Signed-off-by: Faiz Jazadi <me@lcat.dev>

--- seatup/src/component/Homepage.jsx
+++ seatup/src/component/Homepage.jsx
@@ -1,10 +1,17 @@
-const Homepage = ({text, backdrop}) => {
-    return (
-        <div className="bg-dark p-5 hero-container">
-            <h1 className="hero-text">{text}</h1>
-            {backdrop && <div className="hero-backdrop" style={{backgroundImage: `url(${backdrop})`}}></div>}
+const Homepage = () => {
+  return (
+    <div className="p-5 flex">
+      <div className="flex-1">
+        <div className="text-center text-3xl font-bold">
+          Halo, mari kita lihat menu kita hari ini!
+          <div className="bg-black py-7 mt-5 rounded-lg bg-seatup-red text-white text-5xl">
+          Menu Hari Ini
+          </div>
         </div>
-    )
+      </div>
+      <div className="flex-1">Helo</div>
+    </div>
+  )
 }
 
 export default Homepage;
