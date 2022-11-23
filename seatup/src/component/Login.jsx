4e94d29bca98713cfb5b75f3e20371750630b80c
Rachel Naragifta <rachel.naragifta@mail.ugm.ac.id>
2022-11-23 22:26:10 +0700

    finished login page

--- seatup/src/component/Login.jsx
+++ seatup/src/component/Login.jsx
@@ -2,6 +2,8 @@ import email from "../img/login_em.png"
 import password from "../img/login_password.png"
 import passOn from "../img/login_pass.svg"
 import passOff from "../img/login_pass_no.svg"
+import googleIcon from "../img/login_google.png"
+import ugmIcon from "../img/login_ugm.png"
 import { useState } from 'react'
 
 const Login = () => {
@@ -23,7 +25,7 @@ const Login = () => {
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <img src={email}></img>
                 </span>
-                <input type="email" name="email" id="email" className="rounded-full  bg-broken2white text-gray-900 h-12 w-full pl-10 pr-36 p-2.5 focus:outline-none " placeholder="Email" required=""></input>
+                <input type="email" name="email" id="email" className="rounded-full  bg-abu text-gray-900 h-12 w-full pl-10 pr-36 p-2.5 focus:outline-none " placeholder="Email" required=""></input>
               </label>
             </form>
           </div>
@@ -33,7 +35,7 @@ const Login = () => {
                 <span className="absolute inset-y-0 left-0 flex items-center pl-4">
                   <img src={password}></img>
                 </span>
-                <input type={(open === false) ? 'password' : 'text'} name="password" id="password" className="rounded-full px-4 py-2 bg-broken2white text-gray-900 h-12 w-full pl-10 pr-36 p-2.5 focus:outline-none " placeholder="Kata Sandi" required=""></input>
+                <input type={(open === false) ? 'password' : 'text'} name="password" id="password" className="rounded-full px-4 py-2 bg-abu text-gray-900 h-12 w-full pl-10 pr-36 p-2.5 focus:outline-none " placeholder="Kata Sandi" required=""></input>
                 <div className="absolute inset-y-0 right-4  flex items-center text-sm leading-5">
                   <div>
                     {
@@ -48,19 +50,43 @@ const Login = () => {
           <div className="flex justify-between w-full" >
             <div class="flex items-center ">
               <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"></input>
-              <label for="default-checkbox" class="ml-2 font-medium text-gray-900">Ingat Saya</label>
+              <label for="default-checkbox" class="ml-2  text-gray-900">Ingat Saya</label>
             </div>
             <a className="font-bold text-merah" href="#"> Lupa Kata Sandi?</a>
           </div>
-          <div class="relative flex  items-center">
-            <div class="flex-grow border-b-4 border-gray-200"></div>
-            <span class="flex-shrink mx-4 ">atau masuk dengan</span>
-            <div class="flex-grow border-b04 border-gray-200"></div>
+          <div>
+            <button type="button" class="text-black hover:bg-kuningtua bg-kuning rounded-full px-6 py-3 font-extrabold  drop-shadow-lg  text-center ">Masuk</button>
+          </div>
+          <div className="relative flex  items-center w-full">
+            <div className="flex-grow border-t border-darkerabu"></div>
+            <span className="flex-shrink mx-4 ">atau masuk dengan</span>
+            <div className="flex-grow border-t border-darkerabu"></div>
+          </div>
+
+
+          <div class="w-full flex items-center" >
+            <button type="button" className="flex flex-row hover:bg-darkerabu items-center justify-center w-full px-4 py-2 text-black bg-white rounded-full  text-center  outline outline-2 outline-darkerabu">
+              <div className="flex basis-5/12 items-center justify-center w-full">
+                 <img src={googleIcon} className="w-9"></img>
+              </div>
+              <div className="flex basis-7/12">
+                Google
+              </div>
+            </button>
+          </div>
+
+          <div class="w-full flex items-center " >
+            <button type="button" className="flex flex-row  hover:bg-darkbiru items-center justify-center w-full px-4 py-2 text-white bg-biru rounded-full  text-center  outline outline-2 outline-darkerabu font-bold">
+                    <div className="flex basis-4/12 items-center justify-center w-full pl-8">
+                    <img src={ugmIcon} className="w-9"></img>
+                    </div>
+                    <div className="flex basis-8/12 pl-6 ">SSO UGM</div>
+            </button>
           </div>
 
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 
--- seatup/tailwind.config.js
+++ seatup/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
       'white': '#FFFFFF',
       'black': '#000000',
       'brokenwhite': '#F6F6F6',
-      'broken2white': '#DADADA'
+      'broken2white': '#DADADA',
+      'darkerabu':'#AAAAAA',
+      'kuningtua': '#D99B40',
+      'biru': '#223954',
+      'darkbiru':'#111E2E'
     }
   },
   plugins: [],
