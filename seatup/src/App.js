ce235c3a350cd6778e6b0f00544ff01956a802c2
kadek <kadeknindap@gmail.com>
2022-11-25 21:52:15 +0700

    Add notification page

--- seatup/src/App.js
+++ seatup/src/App.js
@@ -11,34 +11,9 @@ import Checkout from './component/Checkout';
 import Error from './component/Error';
 import Register from './component/Register';
 import LoadingPayment from './component/LoadingPayment';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+import Notification from './component/NotificationPage'
+import { useState } from 'react';
 import NotificationPage from './component/NotificationPage';
-import Login from './component/Login';
-=======
-import Login from './component/Login';
-=======
->>>>>>> 33223b1 ([edit]: App)
-=======
->>>>>>> 65b2300 (hotfix: Duplicate import)
-=======
-import Login from './component/Login';
->>>>>>> 520af09 ([edit]: login)
-=======
->>>>>>> bca67e3 ([edit]: App)
-import { useState } from 'react';
->>>>>>> 3109a91 ([edit]: login)
-=======
-import Login from './component/Login';
-=======
->>>>>>> bd27cc8 ([edit]: App)
-import { useState } from 'react';
->>>>>>> 3eaa541 ([edit]: login)
 
 
 function App() {
@@ -52,63 +27,14 @@ function App() {
         <Route path='/checkout' element={<Checkout />} />
         <Route path='/profil' element={<Profil />} />
         <Route path='/seat' element={<Seat />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Route path='/cart' element={<Cart />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 65b2300 (hotfix: Duplicate import)
-        <Route path='/checkout' element={<Checkout />} />
->>>>>>> 33223b1 ([edit]: App)
-        <Route path='/loading-payment' element={<LoadingPayment />} />
-<<<<<<< HEAD
-        <Route path='/notification' element={<NotificationPage/>} />
-=======
-=======
->>>>>>> bca67e3 ([edit]: App)
-=======
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path='/loading-payment' element={<LoadingPayment />} />
->>>>>>> a28bc49 (hotfix: Duplicate import)
-        <Route path='/login' element={login ? <Profil /> : <Login setLogin={setLogin}/>} />
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 3109a91 ([edit]: login)
-=======
-=======
-        <Route psath='/cart' element={<Cart />} />
->>>>>>> bd27cc8 ([edit]: App)
-        <Route path='/loading-payment' element={<LoadingPayment />}/>
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
->>>>>>> 036fc17 (added register page and paths)
-=======
-        <Route path='/loading-payment' element={<LoadingPayment />}/>
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
->>>>>>> 231c302 (added register page and paths)
-=======
->>>>>>> 33223b1 ([edit]: App)
-=======
-        <Route path='/loading-payment' element={<LoadingPayment />}/>
-        <Route path='/register' element={<Register />} />
->>>>>>> 65b2300 (hotfix: Duplicate import)
-=======
         <Route path='/cart' element={<Cart />} />
         <Route path='/checkout' element={<Checkout />} />
         <Route path='/loading-payment' element={<LoadingPayment />} />
         <Route path='/login' element={login ? <Profil /> : <Login setLogin={setLogin}/>} />
         <Route path='/loading-payment' element={<LoadingPayment />}/>
         <Route path='/register' element={<Register />} />
->>>>>>> 3136b57 (hotfix: Duplicate import)
+        <Route path='/notification' element={<NotificationPage />} />
         <Route path="*" element={<Error />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- seatup/tailwind.config.js
+++ seatup/tailwind.config.js
@@ -2,29 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,html}"],
   theme: {
-    extend: {},
-    colors: {
-      'kuning': '#FFAC30',
-      'kuning2': '#FFE9C7',
-      'merah': '#A7332F',
-      'abu': '#DADADA',
-      'white': '#FFFFFF',
-      'black': '#000000',
-      'brokenwhite': '#F6F6F6',
-      'broken2white': '#DADADA',
-      'abu2':'#9F9F9F',
-      'darkerabu':'#AAAAAA',
-      'kuningtua': '#D99B40',
-      'biru': '#223954',
-      'darkbiru':'#111E2E'
-    },
     extend: {
       colors: {
         'kuning': '#FFAC30',
         'merah': '#A7332F',
         'abu': '#DADADA',
         'white': '#FFFFFF',
-        'black': '#000000'
+        'black': '#000000',
+        'brokenwhite': '#F6F6F6',
+        'broken2white': '#DADADA',
+        'darkerabu':'#AAAAAA',
+        'kuningtua': '#D99B40',
+        'biru': '#223954',
+        'darkbiru':'#111E2E',
+        'kuning2': '#FFE9C7',
+        'abu2':'#9F9F9F'
       }
     },
   },
